c3125a821b2ac2730dffa7b766c0d0f9312dca1d
Rahul Mishra <86767776+TECH-BABA@users.noreply.github.com>
2022-01-23 14:59:20 +0545

    Update cr4ck.py

--- cr4ck.py
+++ cr4ck.py
@@ -1,56 +1,17 @@
 #coding=utf-8
 #!/usr/bin/python2
-#This Script Write By Tech Baba
-#Do Not Copy This Script :)
-try:
-    import os
-    import sys
-    import time
-    import urllib
-    import platform
-    import requests
-    import base64
-except ImportError:
-    os.system('pip2 install requests') #Module Not found Eror :(
+import os
+import random
+import sys
+os.system('rm -rf .txt')
+for n in range(5999):
+    nmbr = random.randint(1111111, 9999999)
+    sys.stdout = open('.txt', 'a')
+    print nmbr
+    sys.stdout.flush()
 
-#Loding Funcation :)
-def baba(word):
-    lix = ["[\033[1;92m■\033[1;97m□□□□□□□□□□□□□]","[\033[1;93m■■\033[1;97m□□□□□□□□□□□□]", "[\033[1;94m■■■\033[1;97m□□□□□□□□□□□]", "[\033[1;96m■■■■\033[1;97m□□□□□□□□□□]", "[\033[1;95m■■■■■\033[1;97m□□□□□□□□□]", "[\033[1;97m■■■■■■\033[1;97m□□□□□□□□]", "[\033[1;93m■■■■■■■\033[1;97m□□□□□□□]", "[\033[1;91m■■■■■■■■\033[1;97m□□□□□□]", "[\033[1;96m■■■■■■■■■\033[1;97m□□□□□]", "[\033[1;92m■■■■■■■■■■\033[1;97m□□□□]", "[\033[1;94m■■■■■■■■■■■\033[1;97m□□□]", "[\033[1;95m■■■■■■■■■■■■\033[1;97m□□]", "[\033[1;93m■■■■■■■■■■■■■\033[1;97m□]", "[\033[1;92m■■■■■■■■■■■■■■\033[1;97m]"]
-    for i in range(5):
-        for x in range(len(lix)):
-            sys.stdout.write(('\r{}{}').format(str(word), lix[x]))
-            time.sleep(0.09)
-            sys.stdout.flush()
-
-logo_base = 'G1swOzk3bQkkJCQkJCQkJCAgJCQkJCQkJCQgICQkICAgICAkJAoJJCQgICAgICQkICQkICAgICAkJCAgJCQgICAkJAoJJCQgICAgICQkICQkICAgICAkJCAgICQkICQkCgkkJCQkJCQkJCAgJCQgICAgICQkICAgICQkJAoJJCQgICAkJCAgICQkICAgICAkJCAgICQkICQkCgkkJCAgICAkJCAgJCQgICAgICQkICAkJCAgICQkCgkkJCAgICAgJCQgJCQkJCQkJCQgICQkICAgICAkJAotLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLQogfHwgG1swOzk3bRtbMDs0MW0gUHJvZ3JhbW1pbmcgaXMgdGhpbmtpbmcgbm90IHR5cGluZyA6KSAbWzBtIHx8Ci0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tChtbMDs5N20g4p6kIEF1dGhvciAgOiBZZXN3YW50IE1pc2hyYQobWzA7OTdtIOKepCBWZXJzaW9uIDogMy4wChtbMDs5N20g4p6kIGZiLXBhZ2UgOiBodHRwczovL2ZiLmNvbS9UZWNobmljYWwuTWlzaHJhCi0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0t'
-logo = base64.b64decode(logo_base) # logo with base64 encodeing :)
-
-def home(): #main menu
-    baba(" Updating Çommand")
-    os.system("clear")
-    arm = platform.architecture()[0] #Checking arm
-    print logo #raw
-    print 47*("-")
-    print('Your Device Is \033[0;92m'+arm).center(45)  #Show Raw
-    print 47*("\033[0;97m-")
-    baba("\tUpdateing => ")
-    os.system("clear")
-    print logo
-    device = platform.architecture()[0]
-    if device == '64bit':
-        from libmainrdx import x_x_x_home_x_x_x
-        x_x_x_home_x_x_x()
-    elif device == '32bit':
-        os.system("clear")
-        os.system("exit")
-        os.system("cd $HOME") #Home 
-        os.system("cat /data/data/com.termux/files/usr/etc/motd") # Show Termux Info Text 
-        print("$ Your Device Has Been No Support :( ")
-    else:
-        os.system("clear")
-        os.system("cat /data/data/com.termux/files/usr/etc/motd") # Show Termux Info Text 
-        os.system("exit")
-
-if __name__ == '__main__':
-    os.system("git pull")
-    home()
+reload(sys)
+os.system("git pull")
+sys.setdefaultencoding('utf8')
+from libmyrdx import reg
+reg()
